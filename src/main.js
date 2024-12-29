dfc8a50230a6e26740c19cbb0d39a7ce5ea6a3dd
kokorau <kokorau@gmail.com>
2024-12-29 21:17:17 +0900

    nav-guard

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+history.replaceState(null, document.title, window.location.href);
+history.pushState(null, document.title, window.location.href);
 
-setupCounter(document.querySelector('#counter'))
+const popupBlocker = document.getElementById("popup-blocker");
+
+window.addEventListener(
+  "popstate",
+  function (event) {
+    popupBlocker.style.display = "flex";
+  },
+  { once: true },
+);
