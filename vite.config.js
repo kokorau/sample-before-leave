0920153feb74afa8782f0fd46262c871a2cc5512
kokorau <kokorau@gmail.com>
2024-12-30 00:25:33 +0900

    action

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "sample-before-leave" : "./",
+  base: process.env.GITHUB_PAGES ? "sample-before-leave" : "aaaa",
   root,
   build: {
     outDir,
