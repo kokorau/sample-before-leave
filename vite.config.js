67d175ec96f9fec86f093e2c82951628e30e417b
kokorau <kokorau@gmail.com>
2024-12-29 23:59:16 +0900

    action

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "REPOSITORY_NAME" : "./",
   root,
   build: {
     outDir,
